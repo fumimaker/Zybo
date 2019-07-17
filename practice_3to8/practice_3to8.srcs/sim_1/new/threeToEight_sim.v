a24715c5cc5fe1a6fd852ab12fe6e2122b8ac28a
fumimaker <fumi01200924@gmail.com>
2019-07-17 19:27:57 +0900

    3to8デコーダー実装できた！

--- practice_3to8/practice_3to8.srcs/sim_1/new/threeToEight_sim.v
+++ practice_3to8/practice_3to8.srcs/sim_1/new/threeToEight_sim.v
@@ -21,15 +21,33 @@
 
 
 module threeToEight_sim();
-reg a[2:0];
-wire y[7:0];
-threeToEight(
+localparam STEP = 8;
+reg [2:0] a;
+wire [7:0] y;
+threeToEight encorder38(
     .a(a),
     .y(y)
 );
 
 initial begin
 #STEP
+a = 3'd0;
+#STEP
+a = 3'd1;
+#STEP
+a = 3'd2;
+#STEP
+a = 3'd3;
+#STEP
+a = 3'd4;
+#STEP
+a = 3'd5;
+#STEP
+a = 3'd6;
+#STEP
+a = 3'd7;
+#STEP
 
+$finish;
 end
 endmodule
