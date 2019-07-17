2857b0498f1f06582e4cd8a3ade57685c780d02a
fumimaker <fumi01200924@gmail.com>
2019-07-17 20:28:58 +0900

    create alu

---
+++ practice_alu/practice_alu.srcs/sim_1/new/alu_sim.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+
+module alu_sim();
+localparam STEP = 8;
+reg CLK;
+reg RST;
+reg [7:0] a,b;
+reg [2:0] s;
+wire [8:0] y;
+
+alu alu_1(
+    .a(a),
+    .b(b),
+    .s(s),
+    .y(y),
+    .CLK(CLK),
+    .RST(RST)
+);
+
+always begin
+    CLK = 0;
+    #(STEP/2);
+    CLK = 1;
+    #(STEP/2);
+end
+
+initial begin
+    RST <= 1;
+    #STEP
+    RST <= 0;
+    #STEP
+    a <= 8'd10;
+    b <= 8'd10;
+    s <= 3'b000;
+    $finish;
+end
+endmodule
---
+++ practice_alu/practice_alu.srcs/sources_1/new/alu.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2019/07/17 19:37:29
+// Design Name: 
+// Module Name: alu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu(
+    input RST, CLK,
+    input [7:0] a,
+    input [7:0] b,
+    input [2:0] s,
+    output reg [8:0] y
+    );
+    
+    always @(posedge CLK)
+    begin
+        if(RST) y <= 0;
+        else begin
+           assign y =   s==3'b000 ? a :
+                        s==3'b000 ? b :
+                        s==3'b000 ? a&b :
+                        s==3'b000 ? a|b :
+                        s==3'b000 ? a<<1 :
+                        s==3'b000 ? a>>1 :
+                        s==3'b000 ? a+b : a-b;
+                        
+        end
+    end
+endmodule
