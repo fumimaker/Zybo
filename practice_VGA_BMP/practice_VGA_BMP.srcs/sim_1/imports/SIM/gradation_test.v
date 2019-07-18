c0db0320c9541bd67e7e02876dc68d0695c22de8
fumimaker <fumi01200924@gmail.com>
2019-07-18 21:55:13 +0900

    制約ファイルのVGAを増やした

---
+++ practice_VGA_BMP/practice_VGA_BMP.srcs/sim_1/imports/SIM/gradation_test.v
@@ -0,0 +1,60 @@
+module vga_bmp_test;
+
+
+localparam STEP = 8;
+localparam CLKNUM = (800*525+12000)*5;
+
+
+reg             CLK;
+reg             RST;
+wire    [3:0]   VGA_R, VGA_G, VGA_B;
+wire            VGA_HS;
+wire            VGA_VS;
+
+vga_bmp vga_bmp_inst(
+    .CLK    (CLK),
+    .RST    (RST),
+    .VGA_R  (VGA_R),
+    .VGA_G  (VGA_G),
+    .VGA_B  (VGA_B),
+    .VGA_HS (VGA_HS),
+    .VGA_VS (VGA_VS)
+);
+
+
+always begin
+    CLK = 0; #(STEP/2);
+    CLK = 1; #(STEP/2);
+end
+
+
+integer fd;  
+
+
+initial begin
+                RST = 0;
+    fd = $fopen("imagedata.raw", "wb");
+    #(STEP*500) RST = 1;
+    #(STEP*10)  RST = 0;
+    #(STEP*CLKNUM);
+    $fclose(fd);
+    $stop;
+end
+
+
+`include "vga_param.vh"
+
+
+wire fileouten = (HFRONT + HWIDTH + HBACK <= vga_bmp.syncgen.HCNT) &&
+                 (VFRONT + VWIDTH + VBACK <= vga_bmp.syncgen.VCNT);
+
+
+always @(posedge vga_bmp.PCK) begin
+    if ( fileouten ) begin
+        $fwrite(fd, "%c", {VGA_R, 4'h0});
+        $fwrite(fd, "%c", {VGA_G, 4'h0});
+        $fwrite(fd, "%c", {VGA_B, 4'h0});
+    end
+end
+
+endmodule
---
+++ practice_VGA_BMP/practice_VGA_BMP.srcs/sources_1/imports/HDL/gradation.v
@@ -0,0 +1,62 @@
+
+module vga_bmp(
+    input               CLK,
+    input               RST,
+    output  reg  [3:0]  VGA_R,
+    output  reg  [3:0]  VGA_G,
+    output  reg  [3:0]  VGA_B,
+    output              VGA_HS,
+    output              VGA_VS
+);
+
+/* VGA(640×480)用パラメータ読み込み */
+`include "vga_param.vh"
+
+localparam HSIZE = 10'd64;
+localparam VSIZE = 10'd120;
+
+/* 同期信号作成回路の接続 */
+wire            PCK;
+wire    [9:0]   HCNT, VCNT;
+
+syncgen syncgen(
+    .CLK    (CLK),
+    .RST    (RST),
+    .PCK    (PCK),
+    .VGA_HS (VGA_HS),
+    .VGA_VS (VGA_VS),
+    .HCNT   (HCNT),
+    .VCNT   (VCNT)
+);
+
+/* RGB出力を作成 */
+wire [9:0] HBLANK = HFRONT + HWIDTH + HBACK;
+wire [9:0] VBLANK = VFRONT + VWIDTH + VBACK;
+
+wire disp_enable = (HBLANK-10'd1 <= HCNT) && (HCNT < HPERIOD-10'd1)
+                && (VBLANK <= VCNT);
+wire [9:0] hcounter = HCNT-HBLANK+10'd1;
+wire [3:0] pattern  = hcounter[5:2];
+wire [9:0] vcounter = (VCNT-VBLANK)/VSIZE;
+
+reg [3:0] R, G, B;
+always @* begin
+    case ( vcounter )
+        3'd0:   begin R = pattern; G = pattern; B = pattern; end
+        3'd1:   begin R = pattern; G = 4'd0;    B = 4'd0;    end
+        3'd2:   begin R = 4'd0;    G = pattern; B = 4'd0;    end
+        3'd3:   begin R = 4'd0;    G = 4'd0;    B = pattern; end
+        default:begin R = 4'd0;    G = 4'd0;    B = 4'd0;    end
+    endcase
+end
+
+always @( posedge PCK ) begin
+    if ( RST )
+        {VGA_R, VGA_G, VGA_B} <= 12'h000;
+    else if ( disp_enable )
+        {VGA_R, VGA_G, VGA_B} <= {R, G, B};
+    else
+        {VGA_R, VGA_G, VGA_B} <= 12'h000;
+end
+
+endmodule
