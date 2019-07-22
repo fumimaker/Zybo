bad1cce4f0fc870b93746b25e3c20d76254af4c9
fumimaker <fumi01200924@gmail.com>
2019-07-22 18:44:38 +0900

    何回も呼ばれてるのかと思ってprev入れてみたけど変わらなかった

--- practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/xsim_webtalk.tcl
+++ practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/fumi/Zybo/practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon Jul 22 18:02:21 2019" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon Jul 22 18:22:21 2019" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.3 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2405991" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "1252601d-111d-4b39-a255-e2c2e18331b0" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "202aaf3b8d744fad8fb26202455cbb4e" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "18" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "21" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i9-8950HK CPU @ 2.90GHz" -context "user_environment"
@@ -36,7 +36,7 @@ webtalk_add_data -client xsim -key Total_Nets -value "0" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Total_Processes -value "251" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Total_Instances -value "9" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Xilinx_HDL_Libraries_Used -value "secureip unimacro_ver unisims_ver " -context "xsim\\usage"
-webtalk_add_data -client xsim -key Compiler_Time -value "2.53_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Compiler_Memory -value "116232_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 4291362752 -regid "" -xml C:/Users/fumi/Zybo/practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/fumi/Zybo/practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/fumi/Zybo/practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Compiler_Time -value "2.28_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Compiler_Memory -value "116120_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 1968279402 -regid "" -xml C:/Users/fumi/Zybo/practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/fumi/Zybo/practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/fumi/Zybo/practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
--- practice_VGA_BMP/practice_VGA_BMP.srcs/sources_1/imports/HDL/gradation.v
+++ practice_VGA_BMP/practice_VGA_BMP.srcs/sources_1/imports/HDL/gradation.v
@@ -54,7 +54,7 @@ wire disp_enable = (HBLANK-10'd1 <= HCNT) && (HCNT < HPERIOD-10'd1)
 
 
 reg [2:0] R, G, B;
-reg [15:0] pixelCnt = 0;
+reg [15:0] pixelCnt = 0, prev = 0;
 reg [8:0] pixel;
 
 always @(posedge PCK) begin
@@ -69,17 +69,13 @@ always @(posedge PCK) begin
         else if(HCNT<480) begin //draw
             EN <= 1;
             addr <= pixelCnt;
-            if(EN) begin
+            if(EN&&prev!=HCNT) begin
                 pixel <= data;
-                /*
-                R <= (pixel&9'b111000000)>>6;
-                G <= (pixel&9'b000111000)>>3;
-                B <= (pixel&9'b000000111)>>0;
-                */
                 R <= pixel[8:6];
                 G <= pixel[5:3];
                 B <= pixel[2:0];
                 EN <= 0;
+                prev <= HCNT;
                 pixelCnt <= pixelCnt + 10'h9;
             end
         end
