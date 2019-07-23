635975b904ae62bd90e9431b984c2b94a8b84e5f
fumimaker <fumi01200924@gmail.com>
2019-07-23 13:54:31 +0900

    update

--- practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/xsim_webtalk.tcl
+++ practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/fumi/Zybo/practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Tue Jul 23 13:26:03 2019" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Tue Jul 23 13:44:40 2019" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.3 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2405991" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "1252601d-111d-4b39-a255-e2c2e18331b0" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "202aaf3b8d744fad8fb26202455cbb4e" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "60" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "61" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i9-8950HK CPU @ 2.90GHz" -context "user_environment"
@@ -22,12 +22,11 @@ webtalk_add_data -client project -key cpu_speed -value "2904 MHz" -context "user
 webtalk_add_data -client project -key total_processors -value "2" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "17.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
-webtalk_add_data -client xsim -key runall -value "true" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "17284080 ns" -context "xsim\\usage"
-webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "82.17_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "14896_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 2301937635 -regid "" -xml C:/Users/fumi/Zybo/practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/fumi/Zybo/practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/fumi/Zybo/practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
+webtalk_add_data -client xsim -key iteration -value "2" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.12_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6884_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2870280613 -regid "" -xml C:/Users/fumi/Zybo/practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/fumi/Zybo/practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/fumi/Zybo/practice_VGA_BMP/practice_VGA_BMP.sim/sim_1/behav/xsim/xsim.dir/vga_bmp_test_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
