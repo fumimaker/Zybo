e5611246848d9acebc96e6629c6ddcab7e7245a0
fumimaker <fumi01200924@gmail.com>
2019-08-01 01:22:17 +0900

    Revert "last"
    This reverts commit 658b28e8fad209d68c1150640f0dc1e538ebb056.

--- practice_adder/practice_adder.runs/impl_1/adder.tcl
+++ practice_adder/practice_adder.runs/impl_1/adder.tcl
@@ -61,13 +61,99 @@ proc step_failed { step } {
 }
 
 
+start_step init_design
+set ACTIVE_STEP init_design
+set rc [catch {
+  create_msg_db init_design.pb
+  create_project -in_memory -part xc7z010clg400-1
+  set_property board_part digilentinc.com:zybo:part0:2.0 [current_project]
+  set_property design_mode GateLvl [current_fileset]
+  set_param project.singleFileAddWarning.threshold 0
+  set_property webtalk.parent_dir C:/Users/fumi/Zybo/practice_adder/practice_adder.cache/wt [current_project]
+  set_property parent.project_path C:/Users/fumi/Zybo/practice_adder/practice_adder.xpr [current_project]
+  set_property ip_output_repo C:/Users/fumi/Zybo/practice_adder/practice_adder.cache/ip [current_project]
+  set_property ip_cache_permissions {read write} [current_project]
+  add_files -quiet C:/Users/fumi/Zybo/practice_adder/practice_adder.runs/synth_1/adder.dcp
+  read_xdc C:/Users/fumi/Zybo/practice_adder/practice_adder.srcs/constrs_1/imports/PYNQ-Z1/PYNQ_constraints.xdc
+  link_design -top adder -part xc7z010clg400-1
+  close_msg_db -file init_design.pb
+} RESULT]
+if {$rc} {
+  step_failed init_design
+  return -code error $RESULT
+} else {
+  end_step init_design
+  unset ACTIVE_STEP 
+}
+
+start_step opt_design
+set ACTIVE_STEP opt_design
+set rc [catch {
+  create_msg_db opt_design.pb
+  opt_design 
+  write_checkpoint -force adder_opt.dcp
+  create_report "impl_1_opt_report_drc_0" "report_drc -file adder_drc_opted.rpt -pb adder_drc_opted.pb -rpx adder_drc_opted.rpx"
+  close_msg_db -file opt_design.pb
+} RESULT]
+if {$rc} {
+  step_failed opt_design
+  return -code error $RESULT
+} else {
+  end_step opt_design
+  unset ACTIVE_STEP 
+}
+
+start_step place_design
+set ACTIVE_STEP place_design
+set rc [catch {
+  create_msg_db place_design.pb
+  if { [llength [get_debug_cores -quiet] ] > 0 }  { 
+    implement_debug_core 
+  } 
+  place_design 
+  write_checkpoint -force adder_placed.dcp
+  create_report "impl_1_place_report_io_0" "report_io -file adder_io_placed.rpt"
+  create_report "impl_1_place_report_utilization_0" "report_utilization -file adder_utilization_placed.rpt -pb adder_utilization_placed.pb"
+  create_report "impl_1_place_report_control_sets_0" "report_control_sets -verbose -file adder_control_sets_placed.rpt"
+  close_msg_db -file place_design.pb
+} RESULT]
+if {$rc} {
+  step_failed place_design
+  return -code error $RESULT
+} else {
+  end_step place_design
+  unset ACTIVE_STEP 
+}
+
+start_step route_design
+set ACTIVE_STEP route_design
+set rc [catch {
+  create_msg_db route_design.pb
+  route_design 
+  write_checkpoint -force adder_routed.dcp
+  create_report "impl_1_route_report_drc_0" "report_drc -file adder_drc_routed.rpt -pb adder_drc_routed.pb -rpx adder_drc_routed.rpx"
+  create_report "impl_1_route_report_methodology_0" "report_methodology -file adder_methodology_drc_routed.rpt -pb adder_methodology_drc_routed.pb -rpx adder_methodology_drc_routed.rpx"
+  create_report "impl_1_route_report_power_0" "report_power -file adder_power_routed.rpt -pb adder_power_summary_routed.pb -rpx adder_power_routed.rpx"
+  create_report "impl_1_route_report_route_status_0" "report_route_status -file adder_route_status.rpt -pb adder_route_status.pb"
+  create_report "impl_1_route_report_timing_summary_0" "report_timing_summary -max_paths 10 -file adder_timing_summary_routed.rpt -pb adder_timing_summary_routed.pb -rpx adder_timing_summary_routed.rpx -warn_on_violation "
+  create_report "impl_1_route_report_incremental_reuse_0" "report_incremental_reuse -file adder_incremental_reuse_routed.rpt"
+  create_report "impl_1_route_report_clock_utilization_0" "report_clock_utilization -file adder_clock_utilization_routed.rpt"
+  create_report "impl_1_route_report_bus_skew_0" "report_bus_skew -warn_on_violation -file adder_bus_skew_routed.rpt -pb adder_bus_skew_routed.pb -rpx adder_bus_skew_routed.rpx"
+  close_msg_db -file route_design.pb
+} RESULT]
+if {$rc} {
+  write_checkpoint -force adder_routed_error.dcp
+  step_failed route_design
+  return -code error $RESULT
+} else {
+  end_step route_design
+  unset ACTIVE_STEP 
+}
+
 start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
-  set_param xicom.use_bs_reader 1
-  open_checkpoint adder_routed.dcp
-  set_property webtalk.parent_dir C:/Users/fumi/Zybo/practice_adder/practice_adder.cache/wt [current_project]
   catch { write_mem_info -force adder.mmi }
   write_bitstream -force adder.bit 
   catch {write_debug_probes -quiet -force adder}
